e7f072e6b6322afa5545e161a9cef10086e2f2dc
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 12:34:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
- programmer_hash[:yukihiro_matsumoto][:known_for => "COBOL"][:languages => "COBOL", "FORTRAN"] = newly-added-to
+ newly-added-to-hash = programmer_hash[:yukihiro_matsumoto][:known_for => "COBOL"][:languages => "COBOL", "FORTRAN"]
 
 end
 
